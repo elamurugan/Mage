8fce031f6b31412e8c60a6664544b541a91b6c3e
Ela <nelamurugan@gmail.com>
2015-02-22 16:19:13 +0530

    Updated shell script to generate .gitignore

--- module-gitignore-generate.sh
+++ module-gitignore-generate.sh
@@ -7,8 +7,8 @@
 SITEGITDIRECTORY=".git"
 SITEGITIGNORE=".gitignore"
 
-SITEBKPGITDIRECTORY=".bkp.git"
-SITEBKPGITIGNORE=".bkp.gitignore"
+SITEBKPGITDIRECTORY=".site.git"
+SITEBKPGITIGNORE=".site.gitignore"
 
 NAMESPACE_SMALLER_CASE=""
 MODULE_SMALLER_CASE=""
@@ -37,7 +37,19 @@ capitalise_first_char ()
 }
 
 #- takes user variables -#
-echo "You are here to generate  .gitignore and new .git repo for a separate magento module. "
+
+
+echo "Choose any of these actions."
+echo "Generate  .gitignore for a separate magento module: 1"
+echo "Move separate magento module git and put back site repo: 2"
+echo "Move site repo  git and put back  separate magento module: 3"
+echo "Exit: 4"
+
+echo "Enter your action (Proceed if you know what you are doing):"
+read USERACTION
+
+if [ $USERACTION -ne "4" ]
+then
 
 echo "Enter namespace (Case sensitive):"
 read NAMESPACE
@@ -48,33 +60,43 @@ read MODULENAME
 echo "Enter codepool (Case sensitive)  Eg. local or community:"
 read CODEPOOL
 
-echo "Want to create default directories? Y/N:"
-read DEFAULTDIRECTORIES
+
+fi
 
 
-NAMESPACE_SMALLER_CASE=$(to_lowercase $MODULENAME)
+NAMESPACE_SMALLER_CASE=$(to_lowercase $NAMESPACE)
 MODULE_SMALLER_CASE=$(to_lowercase $MODULENAME)
 CODEPOOL=$(to_lowercase $CODEPOOL)
 
-NAMESPACE=$(to_lowercase $NAMESPACE)
-MODULENAME=$(to_lowercase $MODULENAME)
+#NAMESPACE=$(to_lowercase $NAMESPACE)
+#MODULENAME=$(to_lowercase $MODULENAME)
 
-NAMESPACE=$(capitalise_first_char $NAMESPACE)
-MODULENAME=$(capitalise_first_char $MODULENAME)
+#NAMESPACE=$(capitalise_first_char $NAMESPACE)
+#MODULENAME=$(capitalise_first_char $MODULENAME)
 
-if [[ -d "${SITEGITDIRECTORY}" && ! -L "${SITEGITDIRECTORY}" ]] ; then
+case $USERACTION in
+	"1")
+			
+echo "Want to create default directories?[Make sure to check folder permissions after creating] Y/N:"
+read DEFAULTDIRECTORIES
+
+
+
+if [ ! -d "${SITEBKPGITDIRECTORY}" ] && [ ! -f  "${SITEBKPGITIGNORE}" ]
+then
+if [ -d "${SITEGITDIRECTORY}" ] ; then
     mv ${SITEGITDIRECTORY} ${SITEBKPGITDIRECTORY}
 	echo "!!##YOUR CURRENT  ${SITEGITDIRECTORY} HAS BEEN RENAMED to ${SITEBKPGITDIRECTORY} ##!!"
 fi
 
-if [ -fi"${SITEGITIGNORE}" ]
+if [ -f "${SITEGITIGNORE}" ]
 then
     mv ${SITEGITIGNORE} ${SITEBKPGITIGNORE}
 	echo "!!##YOUR CURRENT ${SITEGITIGNORE} HAS BEEN RENAMED to ${SITEBKPGITIGNORE} ##!!"
 fi
 
 # creates default needed directories #
-if [[ $DEFAULTDIRECTORIES = "Y"  || $DEFAULTDIRECTORIES = "y" ]]
+if [ "${DEFAULTDIRECTORIES}"="Y" ] && [ "${DEFAULTDIRECTORIES}"="y" ]
 then
 	
 mkdir -p app/code/$CODEPOOL/$NAMESPACE/$MODULENAME/Block/
@@ -102,6 +124,7 @@ fi
 
 
 touch $SITEGITIGNORE
+touch "${NAMESPACE_SMALLER_CASE}_${MODULE_SMALLER_CASE}_readme.md"
 
 cat > $SITEGITIGNORE << EOF
 /*
@@ -156,9 +179,9 @@ cat > $SITEGITIGNORE << EOF
 /app/etc/*
 !app/etc/modules
 /app/etc/modules/*
-!app/etc/modules/$NAMESPACE_$MODULENAME.xml
+!app/etc/modules/${NAMESPACE}_${MODULENAME}.xml
 
-!lib/
+!/lib/
 /lib/*
 !/lib/$NAMESPACE_SMALLER_CASE/
 /lib/$NAMESPACE_SMALLER_CASE/*
@@ -191,23 +214,22 @@ cat > $SITEGITIGNORE << EOF
 !/shell/
 /shell/*
  
-!$NAMESPACE_SMALLER_CASE_$MODULE_SMALLER_CASE_readme.md
+!${NAMESPACE_SMALLER_CASE}_${MODULE_SMALLER_CASE}_readme.md
 !.gitignore
 
 
 var/
 downloader/
-errors/
-includes/
+/errors/
+/includes/
 .idea/
-.htaccess
 .htaccess.sample
 .phpstorm.meta.php
 LICENSE.html
 LICENSE.txt
 LICENSE_AFL.txt
 RELEASE_NOTES.txt
-api.php
+/api.php
 mage
 php.ini.sample
 pkginfo/
@@ -223,4 +245,44 @@ install.php
 EOF
 
 
-echo "$SITEGITIGNORE for your $NAMESPACE_$MODULENAME generated."
+echo "${SITEGITIGNORE} for your ${NAMESPACE}_${MODULENAME} generated."
+else 
+echo "${SITEBKPGITDIRECTORY} or  ${SITEBKPGITIGNORE} already exist, please check to keep it safe."
+fi
+
+;;
+
+	"2")
+			echo "Moving module git"
+			
+			if [ ! -d ".${NAMESPACE_SMALLER_CASE}_${MODULE_SMALLER_CASE}.git" ] && [ ! -f  ".${NAMESPACE_SMALLER_CASE}_${MODULE_SMALLER_CASE}.gitignore" ]
+			then
+			mv ${SITEGITDIRECTORY} ".${NAMESPACE_SMALLER_CASE}_${MODULE_SMALLER_CASE}.git"
+			mv ${SITEGITIGNORE} ".${NAMESPACE_SMALLER_CASE}_${MODULE_SMALLER_CASE}.gitignore"
+			
+			mv ${SITEBKPGITDIRECTORY}  ${SITEGITDIRECTORY}
+			mv ${SITEBKPGITIGNORE}  ${SITEGITIGNORE}
+			
+			else 
+			echo "${NAMESPACE_SMALLER_CASE}_${MODULE_SMALLER_CASE} related git files already exist, please check to keep it safe."
+			fi
+			;;
+    "3")
+			echo "Moving magento git for to pull/fetch the module to update"
+			if [ ! -d "${SITEBKPGITDIRECTORY}" ] && [ ! -f  "${SITEBKPGITIGNORE}" ]
+			then
+			mv  ${SITEGITDIRECTORY} ${SITEBKPGITDIRECTORY}
+			mv  ${SITEGITIGNORE} ${SITEBKPGITIGNORE}
+			
+			mv  ".${NAMESPACE_SMALLER_CASE}_${MODULE_SMALLER_CASE}.git" ${SITEGITDIRECTORY}
+			mv  ".${NAMESPACE_SMALLER_CASE}_${MODULE_SMALLER_CASE}.gitignore" ${SITEGITIGNORE}
+			
+			else 
+			echo "${SITEBKPGITDIRECTORY} or  ${SITEBKPGITIGNORE} already exist, please check to keep it safe."
+			fi
+			;;
+	 "4")
+			echo "Bye"
+			
+			;;
+esac
